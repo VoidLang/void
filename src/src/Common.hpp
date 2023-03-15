c531094a381a57132db3f03f41e33de77cd4b02c
AdvancedAntiSkid <abel.herfort@gmail.com>
2023-03-15 15:48:11 +0100

    feat(core): project setup

---
+++ src/src/Common.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+#define String std::string
---
+++ src/src/Main.cpp
@@ -0,0 +1,39 @@
+#include "Common.hpp"
+
+/**
+ * Hey there,
+ *  Welcome to the Void.
+ *  I am Ábel (aka AdvancedAntiSkid) the founder of Void.
+ *  If you have any questions feel free to join my discord.
+ *    https://discord.gg/HdgnHqqGAW
+ *
+ * What is Void?
+ *  Void is a dynamic, stack-based c++ virtual machine and compiler that I started making
+ *  for educational purposes. In my free time I always aim towards learning as much as possible,
+ *  and once I thought how cool it would be to have my own programming language. Well here it is.
+ * Why Void?
+ *  Void's vision is to provide an easy, understandable programming language for both
+ *	educational and production purposes.
+ * How stable is Void?
+ *  According to my testings Void should be pretty stable, but keep in mind that I consider myself
+ *  as a c++ beginner, therefore I'm positive most parts of Void could be written in so much easier ways.
+ *  If you have encountered any bug smake sure to report them on my discord.
+ * Can I contribute?
+ *  Sure. Just make sure to write clean code, follow the code scheme, and comment out everything.
+ *
+ * For further information, please visit https://github.com/VoidLang/Void
+ *
+ * Written at: 10/08/2021 07:39 (MM-dd-yyyy HH-mm)
+ * Last updated at: 03/15/2023 07:39 (MM-dd-yyyy HH-mm)
+ */
+
+
+/**
+ * The current version of the build.
+ */
+const String VERSION = "1.0.1-BETA";
+
+int main() {
+    std::cout << "Hello, World!\n";
+    return 0;
+}
